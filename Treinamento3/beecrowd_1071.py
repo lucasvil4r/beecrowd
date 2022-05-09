59e10b09031c23ce668f76499e722328e3225b9c
lucasvil4r <90861563+lucasvil4r@users.noreply.github.com>
2022-05-08 22:31:11 -0300

    Resolução

--- Treinamento3/beecrowd_1071.py
+++ Treinamento3/beecrowd_1071.py
@@ -6,9 +6,11 @@ if (num >= 5 and num <= 2000):
         for list in range(num, 0, -2):
             num_impares.append(list)
             num_impares.sort(reverse = False)
+
     elif (num % 2 == 1):
         for list in range(num - 1, 0, -2):
             num_impares.append(list)
             num_impares.sort(reverse = False)
+            
 for lista in num_impares:
     print(f'{lista}^2 = {lista ** 2}')
